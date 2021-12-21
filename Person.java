5faabbe599a1380242225a6ab7d7f86a966f5a59
Abood-Yasen <95623488+Abood-Yasen@users.noreply.github.com>
2021-12-21 12:23:00 +0200

    Initial commit

---
+++ Person.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JavaApplication61;
+
+/**
+ *
+ * @author m.s.i
+ */
+class Person {
+
+    int age;
+    String name;
+    
+}
